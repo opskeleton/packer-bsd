8aa6bb110711dbae3549233f4e6f8a9942aae80a
ronen <narkisr@gmail.com>
2014-12-15 00:05:12 +0200

    setting compression off for minimization

--- scripts/common/minimize.sh
+++ scripts/common/minimize.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -eux
 
+zfs set compression=off tank/ROOT/default
+zfs set compression=off tank/root
 dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/root/EMPTY bs=1M
 rm -f /EMPTY
+rm -f /root/EMPTY
+zfs set compression=on tank/ROOT/default
+zfs set compression=on tank/root
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
 sync
